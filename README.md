e56119ebcaf72b16bc24993f71c61b8f70768c12
Marco Stellin <letal32@gmail.com>
2018-03-22 18:27:56 +0000

    RESTful App

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # WebDevBootcamp
 This repositiory will contain all the exercises and projects realized for the WebDev Bootcamp course on Udemy
+
+## Ideas for future projects
+
+* Countdown app with multiple countdowns. Completely frontend using HTML5 storage.
+* Tutorial on Amazon regarding Serverless architecture
---
+++ RESTfulBlogApp/app.js
@@ -0,0 +1,107 @@
+var express          = require("express"),
+    bodyParser       = require("body-parser"),
+    mongoose         = require("mongoose"),
+    methodOverride   = require("method-override"),
+    expressSanitizer = require("express-sanitizer"),
+    app              = express();
+
+//APP CONFIG
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.use(expressSanitizer());
+
+mongoose.connect("mongodb://localhost/restful_blog_app");
+
+//MONGOOSE CONFIG
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+
+// RESTFUL ROUTES
+app.get("/", function(req, res){
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res){
+    Blog.find({}, function(err, blogs){
+        if (err){
+            console.log(err);
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+//NEW ROUTE
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+});
+
+//SHOW ROUTE
+app.get("/blogs/:id", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+
+//UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
+
+//CREATE ROUTE
+app.post("/blogs", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render("new");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
+app.delete("/blogs/:id", function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if (err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
+app.listen("3000", "localhost", function(){
+    console.log("Server is running...");
+});
+
