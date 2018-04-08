2f289654594e7ef0748f8e2a962b1f0015ae505b
letal32 <letal32@gmail.com>
2018-04-08 23:42:46 +0100

    Associated user model to comments and campgrounds

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -15,7 +15,7 @@ router.get("/", function(req,res){
 });
 
 //NEW - Show form to add new campground
-router.get("/new", function(req,res){
+router.get("/new",isLoggedIn, function(req,res){
     res.render("campgrounds/new");
 });
 
@@ -32,11 +32,15 @@ router.get("/:id", function(req, res){
 });
 
 //CREATE - Add new campground to db
-router.post("/", function(req, res){
+router.post("/", isLoggedIn, function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
-    var newCampground = {name: name, image: image, description: description};
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    };
+    var newCampground = {name: name, image: image, description: description, author: author};
 
     Campground.create(newCampground, function(err, newlyCreated){
         if (err){
@@ -46,5 +50,48 @@ router.post("/", function(req, res){
         }
     });
 });
+
+//EDIT CAMPGROUND ROUTE
+router.get("/:id/edit", function(req,res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if (err){
+            res.redirect("/campgrounds");
+        } else {
+            res.render("campgrounds/edit", {campground: foundCampground});
+        }
+    });
+});
+
+
+//UPDATE
+router.put("/:id", function(req,res){
+    console.log(req.body.campground);
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+        if (err){
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
+//DELETE
+router.delete("/:id", function(req,res){
+    Campground.findByIdAndRemove(req.params.id, function(err){
+        if (err){
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
+
+function isLoggedIn(req,res,next){
+    if (req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 module.exports = router;
